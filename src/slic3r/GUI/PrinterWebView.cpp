b11ef48b81fc164583af96a8246762c72c3afd37
yusufaslan0704 <yusufaslan0704@gmail.com>
2026-03-03 17:37:33 +0300

    Adjust top center icon positioning in PrinterWebView by adding horizontal padding, enhancing layout alignment and visual consistency.

--- src/slic3r/GUI/PrinterWebView.cpp
+++ src/slic3r/GUI/PrinterWebView.cpp
@@ -109,7 +109,7 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
     const int top_h = FromDIP(56);
     const int side_w = FromDIP(56);
     const int side_h = FromDIP(170);
-    const int top_center_x = center_pos + (center_size - top_w) / 2;
+    const int top_center_x = center_pos + (center_size - top_w) / 2 + FromDIP(15);
 
     auto *xy_area = new wxPanel(right_container, wxID_ANY, wxDefaultPosition, wxSize(xy_square, xy_square));
     xy_area->SetMinSize(wxSize(xy_square, xy_square));
