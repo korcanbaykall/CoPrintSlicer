730f575ea26c7bf0a53940731c99eb1a60979900
yusufaslan0704 <yusufaslan0704@gmail.com>
2026-03-03 14:44:35 +0300

    Refactor load_printer_url to skip execution when device tab is detached from print host

--- src/slic3r/GUI/MainFrame.cpp
+++ src/slic3r/GUI/MainFrame.cpp
@@ -3920,31 +3920,14 @@ void MainFrame::load_url(wxString url)
 
 void MainFrame::load_printer_url(wxString url, wxString apikey)
 {
-    BOOST_LOG_TRIVIAL(trace) << "load_printer_url:" << url;
-    auto evt = new LoadPrinterViewEvent(EVT_LOAD_PRINTER_URL, this->GetId());
-    evt->SetString(url);
-    evt->SetAPIkey(apikey);
-    wxQueueEvent(this, evt);
+    BOOST_LOG_TRIVIAL(trace) << "load_printer_url skipped (device tab detached from print host):" << url;
+    (void) apikey;
+    return;
 }
 
 void MainFrame::load_printer_url()
 {
-    PresetBundle &preset_bundle = *wxGetApp().preset_bundle;
-    if (preset_bundle.use_bbl_device_tab())
-        return;
-
-    auto     cfg = preset_bundle.printers.get_edited_preset().config;
-    wxString url = cfg.opt_string("print_host_webui").empty() ? cfg.opt_string("print_host") : cfg.opt_string("print_host_webui");
-    wxString apikey;
-    const auto host_type = cfg.option<ConfigOptionEnum<PrintHostType>>("host_type")->value;
-    if (cfg.has("printhost_apikey") && (host_type == htPrusaLink || host_type == htPrusaConnect))
-        apikey = cfg.opt_string("printhost_apikey");
-    if (!url.empty()) {
-        if (!url.Lower().starts_with("http"))
-            url = wxString::Format("http://%s", url);
-
-        load_printer_url(url, apikey);
-    }
+    return;
 }
 
 bool MainFrame::is_printer_view() const { return m_tabpanel->GetSelection() == TabPosition::tpMonitor; }
