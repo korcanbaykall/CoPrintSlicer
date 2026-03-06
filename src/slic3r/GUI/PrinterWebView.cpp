af0227f5f93e588f58a6dbabb60345f445c1fb3b
yusufaslan0704 <yusufaslan0704@gmail.com>
2026-03-06 16:18:27 +0300

    Refine layout of progress box in PrinterWebView by adjusting vertical padding and adding a spacer, improving visual consistency and overall interface responsiveness.

--- src/slic3r/GUI/PrinterWebView.cpp
+++ src/slic3r/GUI/PrinterWebView.cpp
@@ -58,7 +58,8 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
     progress_row->AddSpacer(FromDIP(27));
     progress_row->Add(progress_box, 1, wxEXPAND);
     progress_row->AddSpacer(FromDIP(28));
-    left_sizer->Add(progress_row, 0, wxEXPAND | wxTOP | wxBOTTOM, FromDIP(12));
+    left_sizer->Add(progress_row, 0, wxEXPAND | wxTOP, FromDIP(5));
+    left_sizer->AddSpacer(FromDIP(12));
 
     left_container->SetSizer(left_sizer);
 
