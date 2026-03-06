be2bfa70f947387ae7f89b5daaf8e05fbba8cac0
yusufaslan0704 <yusufaslan0704@gmail.com>
2026-03-06 10:12:32 +0300

    Refactor icon addition in PrinterWebView to support height scaling, improving layout flexibility and visual consistency.

--- src/slic3r/GUI/PrinterWebView.cpp
+++ src/slic3r/GUI/PrinterWebView.cpp
@@ -111,17 +111,17 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
     const int side_w = FromDIP(90);
     const int side_h = FromDIP(220);
     const int top_center_x = center_pos + (center_size - top_w) / 2 + FromDIP(15);
-    const int side_shift_y = FromDIP(25);
+    const int side_shift_y = FromDIP(38);
 
     auto *xy_area = new wxPanel(right_container, wxID_ANY, wxDefaultPosition, wxSize(xy_square, xy_square));
     xy_area->SetMinSize(wxSize(xy_square, xy_square));
     xy_area->SetMaxSize(wxSize(xy_square, xy_square));
     xy_area->SetBackgroundColour(wxColour(28, 30, 34));
 
-    auto add_axis_icon = [this](wxWindow *parent, const std::string &icon_key, int x, int y, int box_w, int box_h) {
+    auto add_axis_icon = [this](wxWindow *parent, const std::string &icon_key, int x, int y, int box_w, int box_h, bool prefer_height_scale = false) {
         if (icon_key.empty())
             return;
-        const int pad = this->FromDIP(8);
+        const int pad = this->FromDIP(4);
         int holder_w = box_w - pad * 2;
         int holder_h = box_h - pad * 2;
         if (holder_w < this->FromDIP(1)) holder_w = this->FromDIP(1);
@@ -132,7 +132,7 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
 
         auto *sizer = new wxBoxSizer(wxVERTICAL);
         sizer->AddStretchSpacer(1);
-        const int icon_target = holder_w < holder_h ? holder_w : holder_h;
+        const int icon_target = prefer_height_scale ? holder_h : (holder_w < holder_h ? holder_w : holder_h);
         const int icon_px = this->ToDIP(wxSize(0, icon_target)).GetHeight();
         auto bmp = create_scaled_bitmap(icon_key, this, icon_px > 0 ? icon_px : 1);
         auto *icon = new wxStaticBitmap(holder, wxID_ANY, bmp);
@@ -142,8 +142,8 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
     };
 
     add_axis_icon(xy_area, resolve_icon("vector11", ""), top_center_x, center_pos - gap - top_h, top_w, top_h);
-    add_axis_icon(xy_area, resolve_icon("vector10", ""), center_pos - gap - side_w, (xy_square - side_h) / 2 + side_shift_y, side_w, side_h);
-    add_axis_icon(xy_area, resolve_icon("vector12", ""), center_pos + center_size + gap, (xy_square - side_h) / 2 + side_shift_y, side_w, side_h);
+    add_axis_icon(xy_area, resolve_icon("vector10", ""), center_pos - gap - side_w, (xy_square - side_h) / 2 + side_shift_y, side_w, side_h, true);
+    add_axis_icon(xy_area, resolve_icon("vector12", ""), center_pos + center_size + gap, (xy_square - side_h) / 2 + side_shift_y, side_w, side_h, true);
     add_axis_icon(xy_area, resolve_icon("vector13", ""), top_center_x, center_pos + center_size + gap, top_w, top_h);
 
     auto *center_btn = make_btn("", 90, 90, true);
