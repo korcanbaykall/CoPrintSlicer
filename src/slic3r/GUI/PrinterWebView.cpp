8c211815a11003e754650a74bed3155eff894ee1
yusufaslan0704 <yusufaslan0704@gmail.com>
2026-03-10 09:28:52 +0300

    Add estimated finish time display to PrinterWebView
    Introduce a new row in the PrinterWebView to display the estimated finish time for printing. This includes a label and a value field, enhancing user awareness of the print duration. The layout has been adjusted for better visual alignment and consistency within the interface.

--- src/slic3r/GUI/PrinterWebView.cpp
+++ src/slic3r/GUI/PrinterWebView.cpp
@@ -107,6 +107,17 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
     progress_controls_row->Add(stop_icon, 0, wxALIGN_CENTER_VERTICAL);
     controls_col->Add(progress_controls_row, 0, wxEXPAND | wxTOP, FromDIP(55));
 
+    auto *estimated_finish_row = new wxBoxSizer(wxHORIZONTAL);
+    estimated_finish_row->AddStretchSpacer(1);
+    m_estimated_finish_label = new wxStaticText(progress_box, wxID_ANY, _L("Tahmini bitiş süresi:"));
+    m_estimated_finish_label->SetForegroundColour(wxColour(150, 156, 166));
+    m_estimated_finish_value = new wxStaticText(progress_box, wxID_ANY, "N/A");
+    m_estimated_finish_value->SetForegroundColour(wxColour(220, 220, 220));
+    estimated_finish_row->Add(m_estimated_finish_label, 0, wxALIGN_CENTER_VERTICAL);
+    estimated_finish_row->AddSpacer(FromDIP(8));
+    estimated_finish_row->Add(m_estimated_finish_value, 0, wxALIGN_CENTER_VERTICAL);
+    controls_col->Add(estimated_finish_row, 0, wxEXPAND | wxTOP, FromDIP(12));
+
     auto *layer_info_row = new wxBoxSizer(wxHORIZONTAL);
     m_layer_label = new wxStaticText(progress_box, wxID_ANY, _L("Katman:"));
     m_layer_label->SetForegroundColour(wxColour(150, 156, 166));
@@ -119,14 +130,6 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
     layer_info_row->Add(m_layer_printer_value, 0, wxALIGN_CENTER_VERTICAL);
     layer_info_row->AddSpacer(FromDIP(30));
     layer_info_row->Add(m_layer_file_value, 0, wxALIGN_CENTER_VERTICAL);
-    layer_info_row->AddStretchSpacer(1);
-    m_estimated_finish_label = new wxStaticText(progress_box, wxID_ANY, _L("Tahmini bitiş süresi:"));
-    m_estimated_finish_label->SetForegroundColour(wxColour(150, 156, 166));
-    m_estimated_finish_value = new wxStaticText(progress_box, wxID_ANY, "N/A");
-    m_estimated_finish_value->SetForegroundColour(wxColour(220, 220, 220));
-    layer_info_row->Add(m_estimated_finish_label, 0, wxALIGN_CENTER_VERTICAL);
-    layer_info_row->AddSpacer(FromDIP(8));
-    layer_info_row->Add(m_estimated_finish_value, 0, wxALIGN_CENTER_VERTICAL);
     controls_col->Add(layer_info_row, 0, wxTOP, FromDIP(8));
 
     controls_col->AddStretchSpacer(1);
@@ -513,4 +516,5 @@ void PrinterWebView::OnLoaded(wxWebViewEvent &evt)
 
 } // GUI
 } // Slic3r
+
 
