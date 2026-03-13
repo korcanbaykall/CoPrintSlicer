da4f86c2719c5b9049be9302a3487ad66136e8a2
yusufaslan0704 <yusufaslan0704@gmail.com>
2026-03-13 15:10:51 +0300

    Refine layout in PrinterWebView by removing top margin from progress row, enhancing visual alignment and consistency.

--- src/slic3r/GUI/PrinterWebView.cpp
+++ src/slic3r/GUI/PrinterWebView.cpp
@@ -493,7 +493,7 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
     top_row->AddSpacer(FromDIP(20));
     top_row->Add(side_column, 0, wxTOP | wxBOTTOM | wxRIGHT, FromDIP(5));
     left_sizer->Add(top_row, 0, wxEXPAND);
-    left_sizer->Add(progress_row, 0, wxEXPAND | wxTOP, FromDIP(30));
+    left_sizer->Add(progress_row, 0, wxEXPAND);
     left_sizer->AddSpacer(FromDIP(2));
 
     left_container->SetSizer(left_sizer);
