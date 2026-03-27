8a986e732e7c8dc79e022b83ae06221919478892
yusufaslan0704 <yusufaslan0704@gmail.com>
2026-03-27 14:21:19 +0300

    q

--- src/slic3r/GUI/PrinterWebView.cpp
+++ src/slic3r/GUI/PrinterWebView.cpp
@@ -602,7 +602,7 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
     auto *step_bg_sizer = new wxBoxSizer(wxVERTICAL);
     step_bg_sizer->Add(step_row, 0, wxALL, FromDIP(8));
     step_bg->SetSizer(step_bg_sizer);
-    right_sizer->Add(step_bg, 0, wxTOP | wxLEFT | wxRIGHT | wxALIGN_CENTER_HORIZONTAL, FromDIP(12));
+    right_sizer->Add(step_bg, 0, wxTOP | wxLEFT, FromDIP(24));
 
     auto *content_row = new wxBoxSizer(wxHORIZONTAL);
 
@@ -755,11 +755,11 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
 
     auto *right_placeholder_box = new StaticBox(right_container, wxID_ANY);
     const int right_placeholder_width = FromDIP(220);
-    const int right_placeholder_height = FromDIP(240);
+    const int right_placeholder_height = FromDIP(270);
     const int right_placeholder_right_margin = FromDIP(12);
     const int right_placeholder_min_x = FromDIP(510);
     const int right_placeholder_x = std::max(right_placeholder_min_x, right_container_width - right_placeholder_width - right_placeholder_right_margin);
-    right_placeholder_box->SetSize(wxRect(wxPoint(right_placeholder_x, FromDIP(92)), wxSize(right_placeholder_width, right_placeholder_height)));
+    right_placeholder_box->SetSize(wxRect(wxPoint(right_placeholder_x, FromDIP(62)), wxSize(right_placeholder_width, right_placeholder_height)));
     right_placeholder_box->SetMinSize(wxSize(right_placeholder_width, right_placeholder_height));
     right_placeholder_box->SetMaxSize(wxSize(right_placeholder_width, right_placeholder_height));
     right_placeholder_box->SetCornerRadius(FromDIP(12));
