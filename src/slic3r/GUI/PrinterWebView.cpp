55aad5296f040a1f1fb47cbe5ce123c35bf79b4e
yusufaslan0704 <yusufaslan0704@gmail.com>
2026-03-31 09:45:21 +0300

    n

--- src/slic3r/GUI/PrinterWebView.cpp
+++ src/slic3r/GUI/PrinterWebView.cpp
@@ -851,10 +851,10 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
     fan_unit->SetCursor(wxCursor(wxCURSOR_HAND));
 
     const int fan_row_y = FromDIP(140);
-    const int fan_unit_x = FromDIP(152);
+    const int fan_value_x = FromDIP(99);
     const int fan_value_width = fan_value->GetBestSize().GetWidth();
-    fan_unit->SetPosition(wxPoint(fan_unit_x, fan_row_y));
-    fan_value->SetPosition(wxPoint(fan_unit_x - FromDIP(5) - fan_value_width, fan_row_y));
+    fan_value->SetPosition(wxPoint(fan_value_x, fan_row_y));
+    fan_unit->SetPosition(wxPoint(fan_value_x + fan_value_width + FromDIP(5), fan_row_y));
 
     m_fan_popup_button = fan_label;
     auto fan_popup_handler = [this](wxMouseEvent &) { toggle_fan_popup(); };
@@ -867,7 +867,7 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
     speed_label->SetForegroundColour(wxColour(220, 220, 220));
 
     auto *speed_value = new wxStaticText(right_placeholder_box, wxID_ANY, "--");
-    speed_value->SetPosition(wxPoint(FromDIP(162), FromDIP(200)));
+    speed_value->SetPosition(wxPoint(FromDIP(99), FromDIP(200)));
     speed_value->SetForegroundColour(wxColour(220, 220, 220));
 
     auto *right_placeholder_right_border = new wxPanel(right_container, wxID_ANY);
