0929072878d80125cec4d52e5f769266aedfbfd7
yusufaslan0704 <yusufaslan0704@gmail.com>
2026-05-06 14:48:40 +0300

    Refine right placeholder width calculation in PrinterWebView for improved layout responsiveness
    Adjusted the calculation of the right placeholder width in PrinterWebView to enhance the layout's adaptability. This change ensures better alignment and visual consistency of UI components, contributing to a more cohesive user experience.

--- src/slic3r/GUI/PrinterWebView.cpp
+++ src/slic3r/GUI/PrinterWebView.cpp
@@ -1276,7 +1276,7 @@ PrinterWebView::PrinterWebView(wxWindow *parent)
     auto *right_placeholder_box = new StaticBox(right_container, wxID_ANY);
     const int right_placeholder_height = FromDIP(320);
     const int right_placeholder_x = FromDIP(529);
-    const int right_placeholder_width = right_container_width - right_placeholder_x - FromDIP(2);
+    const int right_placeholder_width = right_container_width - right_placeholder_x - FromDIP(8);
     const int right_placeholder_content_width = FromDIP(175);
     right_placeholder_box->SetSize(wxRect(wxPoint(right_placeholder_x, FromDIP(32)), wxSize(right_placeholder_width, right_placeholder_height)));
     right_placeholder_box->SetMinSize(wxSize(right_placeholder_width, right_placeholder_height));
